01060018d57b0f851e28a357ae5d9732cb2f203b
Akash Chauhan <99891799+a1akashchauhan@users.noreply.github.com>
2022-09-14 12:31:57 +0530

    Time: 352 ms (98.50%), Space: 178.7 MB (97.90%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -11,23 +11,20 @@
  */
 class Solution {
 public:
-    int ans=0;
-    
-    void countPaths(TreeNode* root, set<int>s){
-        if(s.find(root->val)!=s.end()) s.erase(root->val);
-        else s.insert(root->val);
-        if(!root->left && !root->right) {
-            if(s.size()<=1) ans++;
+    int ans=0;    
+    void countPaths(TreeNode* root, int path){
+        path^=(1<<root->val);
+        if(!root->left && !root->right){
+            if((path & (path-1))==0) ans++;
             return;
         }
-        if(root->left) countPaths(root->left,s);
-        if(root->right) countPaths(root->right,s);
+        if(root->left) countPaths(root->left,path);
+        if(root->right) countPaths(root->right,path);
     }
     
     int pseudoPalindromicPaths (TreeNode* root) {
-       if(!root) return 0;
-        set<int> s;
-        countPaths(root,s);
+      if(!root) return 0;
+        countPaths(root,0);
         return ans;
     }
 };
