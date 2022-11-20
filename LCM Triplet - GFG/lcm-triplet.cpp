2dc5b0a350d74a95dfa8587263c205bf24e21a5a
Akash Chauhan <99891799+a1akashchauhan@users.noreply.github.com>
2022-11-20 18:53:56 +0530

    Added solution - LeetHub

---
+++ LCM Triplet - GFG/lcm-triplet.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+// Initial Template for C++
+// Back-end complete function Template for C++
+// User function Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long lcmTriplets(long long N) {
+        // code here
+        //max(odd)*max(even)*max(odd)
+        if(N==2 || N==1){
+
+            return N;
+
+        }
+
+        if(N%2==0){
+
+            if(N%3==0){
+
+                return (N-1)*(N-2)*(N-3);
+
+            }
+
+            else{
+
+                return N*(N-1)*(N-3);
+
+            }
+
+           } 
+
+        else{
+
+                return N*(N-1)*(N-2);
+
+            }
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        cin >> N;
+        Solution ob;
+        cout << ob.lcmTriplets(N) << "\n";
+    }
+}
+// } Driver Code Ends
