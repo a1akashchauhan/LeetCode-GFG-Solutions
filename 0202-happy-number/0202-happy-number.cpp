81cda069626e3ca37352325740d98ad3772979ff
Akash Chauhan <99891799+a1akashchauhan@users.noreply.github.com>
2022-12-13 13:20:16 +0530

    Time: 10 ms (9.91%), Space: 6.3 MB (40.01%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
     bool isHappy(int n) {
-//         vector<bool>b(1e9);
-//         b[n]=1;
-//         int y=n;
-//         while(1){
-//             long long k;
-//             while(y){
-//                 int kk= y%10;
-//                 k+=(kk*kk);
-//                 y/=10;
-//             }
-//             y=k;
-//             if(k==1){
-//                 return 1;
-//             }
-//             if(b[y]){
-//                 return 0;
-//             }
-//             b[k]=1;
-            
-//         }
-//         return 1;
-        if(n>5){
-            int sum=0;
-            while(n>0){
-                int rem=n%10;
-                sum=sum+rem*rem;
-                n=n/10; 
+        set<int> myset;
+        int val;
+        int index;
+        
+        while(1)
+        {
+            val = 0;
+            while(n)
+            {
+                index = n%10;
+                val += index*index;
+                n /=10;
             }
-            return isHappy(sum);
+            if(val==1)
+                return true;
+            else if(myset.find(val)!=myset.end())
+                return false;
+            
+            myset.insert(val);
+            n = val;
         }
-        if(n==1)
-            return true;
-        else
-            return false;
+        return false;
         
         
     }
