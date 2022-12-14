ddf2e1ce25fb3f54d15aa456192b02c494e1550c
Akash Chauhan <99891799+a1akashchauhan@users.noreply.github.com>
2022-12-14 22:46:37 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -9,14 +9,10 @@ class Solution{
 	const int mod= 1e9+7;
 	int fun(int n, int t, int a[], vector<vector<int>>&dp){
         if(n==0){
-//             return a[0]==t;
            if(t==0 and a[n]==0){
                return 2;
            }
-            if(t==0){
-                return 1;
-            }
-            if(a[n]==t){
+            if(t==0 or a[0]==t){
                 return 1;
             }
             return 0;
@@ -32,24 +28,6 @@ class Solution{
             take = fun(n-1, t-a[n], a, dp);
         }
         return dp[n][t]=(take%mod+nottake%mod)%mod;
-}
-int tab(int n, int tar,int num[], vector<vector<int>>&dp){
-    for(int i=0;i<n;i++){
-       dp[i][0]=1;
-    }
-    dp[0][num[0]]=1;
-    for(int i=1;i<n;i++){
-        for(int j=1;j<=tar;j++){
-            int nottake= dp[i-1][j];
-            int take=0;
-            if(num[i]<=j){
-                take= dp[i-1][j-num[i]];
-            }
-            dp[i][j]=(take%mod+nottake%mod)%mod;
-
-        }
-    }
-    return dp[n-1][tar];
 }
 
 	int perfectSum(int arr[], int n, int sum)
