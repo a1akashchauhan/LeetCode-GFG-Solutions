72a5db10a26fdb57a94bba4acdf55e2998e98884
Akash Chauhan <99891799+a1akashchauhan@users.noreply.github.com>
2022-12-17 19:33:47 +0530

    Added solution - LeetHub

---
+++ Break a number - GFG/break-a-number.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{   
+public:
+    
+    int waysToBreakNumber(int N){
+        // code here 
+        long long int m = 1000000007; 
+            return ((((N+1)%m)*((N+2)%m))/2)%m;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.waysToBreakNumber(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
