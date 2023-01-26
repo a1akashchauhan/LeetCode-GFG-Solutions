88c1094b64c1d68a4842aa39cd1f447c32c5d762
Akash Chauhan <99891799+a1akashchauhan@users.noreply.github.com>
2023-01-27 02:24:35 +0530

    Time: 2098 ms (5.02%), Space: 292.5 MB (7.99%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int fun(int i, int k, int n, vector<int>&nums, vector<vector<int>>&dp){
+        if(i==n){
+            return 0;
+        }
+        if(dp[i][k+1]!=-1){
+            return dp[i][k+1];
+        }
+        int len= fun(i+1, k, n, nums, dp);
+        
+        if(k==-1 or nums[k]<nums[i]){
+            len= max(len, 1+fun(i+1, i, n, nums, dp));
+        }
+        return dp[i][k+1]=len;
+    }
+    
+    
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1,-1));
+        return fun(0,-1, n, nums, dp);   
+        
+    }
+};
